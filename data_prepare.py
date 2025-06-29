c9cd693d6c6851648f3d7e5c3cf7dbb7f8c7a964
Yushi0618 <162889197+Yushi0618@users.noreply.github.com>
2025-06-29 15:34:35 +0800

    Delete data_prepare.py

--- data_prepare.py
+++
@@ -1,8 +0,0 @@
-from torch_geometric.datasets import TUDataset
-datasets = {
-    'msrc':TUDataset(root='data/',name='MSRC_9'),
-    'enzymes':TUDataset(root='data/', name='ENZYMES')
-}
-
-print(f"MSRC_9 dataset size: {len(datasets['msrc'])}")
-print(f"ENZYMES dataset size: {len(datasets['enzymes'])}")
